9caf0930e732e6d35b2f6f4a6f97be78e0933f60
Artem <shavrov.a.a@edu.mirea.ru>
2025-10-01 18:12:44 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+"""
+Главный модуль приложения "Театральный менеджер".
+Точка входа в программу.
+"""
+import sys
+from PySide6.QtWidgets import QApplication
+from app import MainWindow, LoginDialog
+from logger import Logger
+
+if __name__ == "__main__":
+    # Инициализация логгера
+    logger = Logger()
+    logger.info("Запуск приложения")
+
+    # Создание и настройка приложения Qt
+    app = QApplication(sys.argv)
+    app.setApplicationName("Театральный менеджер")
+
+    # Показ диалога авторизации
+    login_dialog = LoginDialog()
+    if login_dialog.exec():
+        # Если авторизация успешна, открываем главное окно
+        window = MainWindow(login_dialog.controller)
+        window.show()
+        sys.exit(app.exec())
+    else:
+        # Если авторизация отменена, выходим из приложения
+        sys.exit(0)
